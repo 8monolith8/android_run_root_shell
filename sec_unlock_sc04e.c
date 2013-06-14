6819f7c5040ebcc8465ee7b478efad6c55e50ae2
fi01 <furu.is01@gmail.com>
2013-06-15 03:21:28 +0900

    Support SC-04E (MF1)

--- sec_unlock_sc04e.c
+++ sec_unlock_sc04e.c
@@ -13,155 +13,113 @@
 
 #include "backdoor_mmap.h"
 
-#define ARRAY_SIZE(x)  (sizeof (x) / sizeof ((x)[0]))
-
-#define reset_security_ops_address                0xc027ec94
-
-#define default_security_ops_address              0xc0fa6804
-#define security_ops_address                      0xc1150630
-
-static const unsigned long int check_reset_security_ops[] = {
-  0xe59f2008, //      LDR     R2, =0xc0fa6804 [$c027eca4] ; default_security_ops
-  0xe59f3008, //      LDR     R3, =0xc1150630 [$c027eca8] ; security_ops
-  0xe5832000, //      STR     R2, [R3]
-  0xe12fff1e, //      BX      LR
-  default_security_ops_address,
-  security_ops_address,
-};
-
-#define sec_restrict_uid_address                  0xc00859ac
-
-static const unsigned long int check_sec_restrict_uid[] = {
-  0xe92d40f7, //      STMPW   [SP], { R0-R2, R4-R7, LR }
-  0xe59f012c, //      LDR     R0, =0xc0f06040 [$c0085ae4] ; tasklist_lock
-  0xeb1de652, //      BL      $c07ff304                   ; _raw_read_lock
-};
-
-#define sec_check_execpath_address                0xc0011550
-
-static const unsigned long int check_sec_check_execpath[] = {
-  0xe2503000, //      SUBS    R3, R0, #$0
-  0xe92d41f0, //      STMPW   [SP], { R4-R8, LR }
-  0xe1a06001, //      MOV     R6, R1
-  0x01a06003, //      MOVEQ   R6, R3
-  0x0a000031, //      BEQ     $c001162c
-  0xeb017e97, //      BL      $c0070fc8                   ; get_mm_exe_file
-};
-
-#define sys_execve_address                        0xc00116b0
-
-static const unsigned long int check_sys_execve[] = {
-  0xe92d4ff0, //      STMPW   [SP], { R4-R11, LR }
-  0xe24dd014, //      SUB     SP, SP, #$14
-  0xe1a05003, //      MOV     R5, R3
-  0xe1a06002, //      MOV     R6, R2
-  0xe58d100c, //      STR     R1, [SP, #$c]
-  0xeb04b560, //      BL      $c013ec4c                   ; getname
-  0xe3700a01, //      CMNS    R0, #$1000
-  0xe1a04000, //      MOV     R4, R0
-  0x81a05000, //      MOVHI   R5, R0
-  0x8a00009e, //      BHI     $c0011954                   ; IS_ERR(filename)
-  0xe1a0200d, //      MOV     R2, SP
-  0xe3c23d7f, //      BIC     R3, R2, #$1fc0
-  0xe3c3303f, //      BIC     R3, R3, #$3f
-  0xe593300c, //      LDR     R3, [R3, #$c]
-  0xe5933204, //      LDR     R3, [R3, #$204]
-  0xe5932004, //      LDR     R2, [R3, #$4]
-  0xe3520000, //      CMPS    R2, #$0
-  0x0a00000e, //      BEQ     $c0011734
-  0xe5932008, //      LDR     R2, [R3, #$8]
-  0xe3520000, //      CMPS    R2, #$0
-  0x0a00000b, //      BEQ     $c0011734
-};
-
-#define DEFINE_CHECK(name)  { name##_address, check_##name, sizeof(check_##name) }
-
 struct check_code_t {
   unsigned long int addr;
   const unsigned long int *expected;
-  size_t size;
 };
 
-static struct check_code_t check_code[] =
-{
-  DEFINE_CHECK(reset_security_ops),
-  DEFINE_CHECK(sec_restrict_uid),
-  DEFINE_CHECK(sec_check_execpath),
-  DEFINE_CHECK(sys_execve),
+struct patch_info_t {
+  unsigned long int sec_restrict_uid;
+  unsigned long int sec_check_execpath;
+  unsigned long int sys_execve;
+  unsigned long int security_ops;
+  unsigned long int default_security_ops;
+  const unsigned long int *patched_sys_execve;
 };
 
-static bool
-check_unlock_code(void)
-{
-  unsigned long int *p;
-  int pos;
-  bool ret = true;
+#define BL_REL(pc,func)     (((func) - (pc) - 8) / 4)
 
-  for (pos = 0; pos < ARRAY_SIZE(check_code); pos++) {
-    p = backdoor_convert_to_mmaped_address((void *)check_code[pos].addr);
+#define VERNAME(NAME)       NAME##_MDI
+#define reset_security_ops_address    0xc027ec94
+#define default_security_ops_address  0xc0fa6804
+#define security_ops_address          0xc1150630
+#define sec_restrict_uid_address      0xc00859ac
+#define sec_check_execpath_address    0xc0011550
+#define sys_execve_address            0xc00116b0
+#define _raw_read_lock_address        0xc07ff304
+#define get_mm_exe_file_address       0xc0070fc8
+#define getname_address               0xc013ec4c
+#define do_execve_address             0xc013b1d0
+#define putname_address               0xc013ec58
+#include "sec_unlock_sc04e.h"
 
-    if (memcmp(p, check_code[pos].expected, check_code[pos].size) != 0) {
-      int i;
-
-      printf("kernel code doesn't match at 0x%08x !!\n", check_code[pos].addr);
-      for (i = 0; i < check_code[pos].size / sizeof (check_code[pos].expected[0]); i++) {
-        printf("  0x%08x\n", p[i]);
-      }
-
-      printf("\n");
-
-      ret = false;
-    }
-  }
-
-  return ret;
-}
+#define VERNAME(NAME)       NAME##_MF1
+#define reset_security_ops_address    0xc027ecfc
+#define default_security_ops_address  0xc0fa6844
+#define security_ops_address          0xc1150670
+#define sec_restrict_uid_address      0xc0085a14
+#define sec_check_execpath_address    0xc0011550
+#define sys_execve_address            0xc00116b0
+#define _raw_read_lock_address        0xc07ff394
+#define get_mm_exe_file_address       0xc0071020
+#define getname_address               0xc013ecb4
+#define do_execve_address             0xc013b238
+#define putname_address               0xc013ecc0
+#include "sec_unlock_sc04e.h"
 
 static const unsigned long int return_zero[] = {
   0xe3a00000, //    MOV     R0, #$0
   0xe12fff1e, //    BX      LR
 };
 
-static const unsigned long int patched_sys_execve[] = {
-  0xe92d4ff0, //      STMPW   [SP], { R4-R11, LR }
-  0xe24dd014, //      SUB     SP, SP, #$14
-  0xe1a05003, //      MOV     R5, R3
-  0xe1a06002, //      MOV     R6, R2
-  0xe58d100c, //      STR     R1, [SP, #$c]
-  0xeb04b560, //      BL      $c013ec4c                   ; getname
-  0xe3700a01, //      CMNS    R0, #$1000
-  0xe1a04000, //      MOV     R4, R0
-  0x81a05000, //      MOVHI   R5, R0
-  0x8a00009e, //      BHI     $c0011954
-  0xe1a03005, //      MOV     R3, R5
-  0xe1a00004, //      MOV     R0, R4
-  0xe59d100c, //      LDR     R1, [SP, #$c]
-  0xe1a02006, //      MOV     R2, R6
-  0xeb04a6b8, //      BL      $c013b1d0                   ; do_execve
-  0xe1a05000, //      MOV     R5, R0
-  0xe1a00004, //      MOV     R0, R4
-  0xeb04b557, //      BL      $c013ec58                   ; putname
-  0xe1a00005, //      MOV     R0, R5
-  0xe28dd014, //      ADD     SP, SP, #$14
-  0xe8bd8ff0, //      LDMUW   [SP], { R4-R11, PC }
-};
-
-static void
-do_patch(void)
+static bool
+check_unlock_code(struct check_code_t *check_code, const char *version)
 {
   unsigned long int *p;
+  int pos;
+  int len;
+  bool ret = true;
 
-  p = backdoor_convert_to_mmaped_address((void *)sec_restrict_uid_address);
+  for (pos = 0; check_code[pos].addr; pos++) {
+    p = backdoor_convert_to_mmaped_address((void *)check_code[pos].addr);
+
+    for (len = 0; check_code[pos].expected[len]; len++) {
+      ;
+    }
+
+    if (memcmp(p, check_code[pos].expected, len * sizeof (check_code[pos].expected[0])) != 0) {
+      int i;
+
+      printf("kernel code doesn't match at 0x%08x for %s!!\n", check_code[pos].addr, version);
+      for (i = 0; i < len; i++) {
+//        printf("  0x%08x -- 0x%08x\n", p[i], check_code[pos].expected[i]);
+      }
+
+//      printf("\n");
+
+      ret = false;
+    }
+  }
+
+  if (ret) {
+    printf("kernel code matched for %s\n", version);
+  }
+
+  return ret;
+}
+
+static void
+do_patch(const struct patch_info_t *info)
+{
+  unsigned long int *p;
+  int len;
+
+  p = backdoor_convert_to_mmaped_address((void *)info->sec_restrict_uid);
   memcpy(p, return_zero, sizeof return_zero);
 
-  p = backdoor_convert_to_mmaped_address((void *)sec_check_execpath_address);
+  p = backdoor_convert_to_mmaped_address((void *)info->sec_check_execpath);
   memcpy(p, return_zero, sizeof return_zero);
 
-  p = backdoor_convert_to_mmaped_address((void *)sys_execve_address);
-  memcpy(p, patched_sys_execve, sizeof patched_sys_execve);
+  p = backdoor_convert_to_mmaped_address((void *)info->sys_execve);
 
-  p = backdoor_convert_to_mmaped_address((void *)security_ops_address);
-  *p = default_security_ops_address;
+  for (len = 0; info->patched_sys_execve[len]; len++) {
+    ;
+  }
+
+  memcpy(p, info->patched_sys_execve, sizeof (info->patched_sys_execve) * len);
+
+  p = backdoor_convert_to_mmaped_address((void *)info->security_ops);
+  *p = info->default_security_ops;
 }
 
 static bool
@@ -176,8 +134,13 @@ do_unlock(void)
     return false;
   }
 
-  if (check_unlock_code()) {
-    do_patch();
+  if (check_unlock_code(check_code_MF1, "MF1")) {
+    do_patch(&patch_info_MF1);
+    ret = true;
+  }
+
+  if (!ret && check_unlock_code(check_code_MDI, "MDI")) {
+    do_patch(&patch_info_MDI);
     ret = true;
   }
 
